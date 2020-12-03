3b4f711f05230f37257de90a4d3a6dfddbf00f59
Ingvar <Dimon_chick@mail.ru>
2020-12-03 15:59:04 +0900

    Lesson_3.2  work is done

---
+++ Lesson_3/lesson_3.2.py
@@ -0,0 +1,10 @@
+def func(**kwargs):
+    return kwargs
+
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ')
+year = input('Введите год рождения: ')
+city = input('Введите город проживания: ')
+email = input('Введите электронный адрес: ')
+tel = input('Введите номер телефона: ')
+print(func(Имя=name, Фамилия=surname, Год_рождения=year, Город_проживания=city, Электронный_адрес=email, Номер_телефона=tel))
