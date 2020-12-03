18dde53596a66e0f3732ff9201d0507f06057360
Ingvar <Dimon_chick@mail.ru>
2020-12-03 15:21:55 +0900

    Lesson_3.1  work is done

--- Lesson_3/Lesson_3.1.py
+++ Lesson_3/Lesson_3.1.py
@@ -1,9 +1,9 @@
 def func(*args):
     return args
 
-num1 = float(input('Введите делимое число: '))
-num2 = float(input('Введите делитель: '))
-if num2 != 0:
-   print(func(num1, 'делим на', num2, 'разность =', num1/num2))
-elif num2 == 0:
+num_1 = float(input('Введите делимое число: '))
+num_2 = float(input('Введите делитель: '))
+if num_2 != 0:
+   print(func(num_1, 'делим на', num_2, 'разность =', num_1/num_2))
+elif num_2 == 0:
    print('На 0 делить нельзя!')
