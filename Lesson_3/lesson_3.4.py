d9f33160290b4d8ac3370994ca7597c0f159d5c4
Ingvar <Dimon_chick@mail.ru>
2020-12-06 17:03:31 +0900

    3.4 second option

--- Lesson_3/lesson_3.4.py
+++ Lesson_3/lesson_3.4.py
@@ -1,4 +1,20 @@
+# Первый вариант решения
+
 def my_func(x, y):
     return x**y
 print(f"{my_func(float(input('Введите действительное число: ')), int(input('Введите целое отрицательное число: ')))}")
 
+
+
+# Второй вариант решения
+'''
+def my_func(x, y):
+    n = 0
+    m = 1
+    while n < abs(y):
+        m = m * (1/x)
+        n = n+1
+    else:
+        return (m)
+print(f"{my_func(float(input('Введите действительное число: ')), int(input('Введите целое отрицательное число: ')))}")
+'''
