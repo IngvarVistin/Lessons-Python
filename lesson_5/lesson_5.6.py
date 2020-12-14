4d874b5c785c476e955128016c9a4166b201f8a3
Ingvar <Dimon_chick@mail.ru>
2020-12-15 01:19:40 +0900

    lesson_5.6 work is done

---
+++ lesson_5/lesson_5.6.py
@@ -0,0 +1,11 @@
+from os import remove
+with open('plan&plan.txt', 'w') as f:
+    f.write('Информатика: 100(л) 50(пр) 210(лаб)\nФизика: 30(л) 0(пр) 10(лаб)\nФизкультура: 0(л) 30(пр) 0(лаб)')
+dict_ = {}
+with open('plan&plan.txt') as f:
+    list_ = f.read().split('\n')
+    for line in list_:
+        line = line.split()
+        subj, lect, pract, lab = line
+        dict_[subj] = int(lect[:-3]) + int(pract[:-4]) + int(lab[:-5])
+    print(f'Общее количество часов по предметам - \n {dict_}')
