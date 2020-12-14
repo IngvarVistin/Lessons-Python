1595af43950c599443dccafd61cca12b30580f20
Ingvar <Dimon_chick@mail.ru>
2020-12-14 15:37:09 +0900

    lesson_5.2 work is done

---
+++ lesson_5/lesson_5.2.py
@@ -0,0 +1,13 @@
+with open('Jonathan Seagull.txt', 'w') as f:
+    f.write('Он понимал, что это триумф!\nПредельная скорость! \nДвести четырнадцать миль в час для чайки! \nЭто был прорыв, незабываемый, неповторимый миг в истории Стаи и начало новой эры в жизни Джонатана.')
+with open('Jonathan Seagull.txt') as f:
+    print(f'Содержимое файла: \n{f.read()}')
+with open('Jonathan Seagull.txt') as f:
+    print(f'Количества строк в файле: {len(f.readlines())}')
+with open('Jonathan Seagull.txt') as f:
+    content = f.readlines()
+    for i in range(len(content)):
+        print(f'Количество символов {i+1} строки: {len(content[i])}')
+with open('Jonathan Seagull.txt') as f:
+    content = f.read().split()
+    print(len(content))
